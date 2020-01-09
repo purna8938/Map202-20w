bdd8677bda4aa06d9c1f69c090b8552a2ca5297b
purnasaikiran <purnasaikiran.ponnam@gmail.com>
2020-01-09 17:08:05 +0000

    first lab

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+$gmail='purnasaikiran.ponnam@gmail.com';
+$collegeId='19018021';
+$name='saikiran';
+$lab='map202';
+?>
+<html>
+	<header>
+		
+	
+	</header>
+	<body>
+		<h1>
+			<?php
+			echo 'my trails';
+				?>
+		</h1>
+		<section>
+				<p>
+				<?php
+			echo $gmail;
+            ?>
+			</p>
+			<p>
+				<?php
+				echo $namename;
+				?>
+			</p>
+			
+				<p>
+					<?php
+			echo $collegeId;
+			
+			
+			?>
+			
+				</p>
+		<p>
+			<?php
+			echo "The time is " . date("h:i:sa");
+			?>
+		</p>
+		<p>
+			<?php
+			echo $lab;
+			?>
+		</p>
+		
+				</section>
+			
+		
+		
+	</body>
+	
+</html>
