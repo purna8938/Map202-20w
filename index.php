acb0c7187eac844e33b24e3e9a0bc198fcf1d897
purnasaikiran <purnasaikiran.ponnam@gmail.com>
2020-01-16 21:28:34 +0000

    second lab

--- index.php
+++ index.php
@@ -1,55 +1,95 @@
+
+
 <?php
-$gmail='purnasaikiran.ponnam@gmail.com';
-$collegeId='19018021';
-$name='saikiran';
-$lab='map202';
+$films = array(
+            "comedy" => array (
+                            0 => "Pink Panther",
+                           1 => "johnny English",
+                            2 => "Tommy Boy"),
+            "action" => array (
+                            0 => "Die Hard",
+                            1 => "Expendables"),
+            "epic" => array (
+                            0 => "The Lord of the Rings"),
+            "Romance" => array (
+                            0 => "Romeo and Juliet")
+);
+	$favorites = array(
+array (
+"Name" => "Dave Punk",
+"Phone" => "5689741523",
+"Email" => "davepunk@gmail.com"),
+array (
+"Name" => "Monty Smith",
+"Phone" => "2584369721",
+"Email" => "montysmith@gmail.com"),
+array (
+"Name" => "John Flinch",
+"Phone" => "9875147536",
+"Email" => "johnflinch@gmail.com")
+);
 ?>
+<!DOCTYPE html>
 <html>
 	<header>
-		
-	
 	</header>
-	<body>
+<body>
+	<section>
 		<h1>
-			<?php
-			echo 'my trails';
-				?>
+			
+			first table
 		</h1>
-		<section>
-				<p>
-				<?php
-			echo $gmail;
-            ?>
-			</p>
-			<p>
-				<?php
-				echo $namename;
-				?>
-			</p>
-			
-				<p>
-					<?php
-			echo $collegeId;
-			
-			
-			?>
-			
-				</p>
-		<p>
-			<?php
-			echo "The time is " . date("h:i:sa");
-			?>
-		</p>
-		<p>
-			<?php
-			echo $lab;
-			?>
-		</p>
-		
-				</section>
-			
-		
-		
-	</body>
-	
+
+<table>
+
+<th> Genre </th>
+<th> Movie Name</th>
+
+<?php
+
+
+ 
+foreach($films as $a => $val) {
+      foreach( $val as $x => $y){
+ echo'<tr>';
+  echo '<td>' . "$a   " .  '</td>';
+		  echo '<td>' . "  $y" .  '</td>';
+  echo'</tr>';
+  }
+}
+
+?>
+</table>
+</section>
+	<h1>
+		second table
+	</h1>
+	<section>
+		<table>
+
+<th> Name </th>
+<th>  Phone </th>
+<th>  Email </th>
+
+<?php
+
+
+ 
+foreach($favorites as $a => $val) {
+    
+ echo'<tr>';
+  echo '<td>' . $val['Name']  .  '</td>';
+  echo '<td>' . $val['Phone']  .  '</td>';
+  echo '<td>' . $val['Email']  .  '</td>';
+
+  echo'</tr>';
+  
+}
+
+?>
+</table>
+
+	</section>
+
+</body>
 </html>
